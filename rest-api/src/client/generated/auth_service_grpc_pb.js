6eb3a9fa53f9af7b7df575e156f20d41d968d454
ppeerttu <perttu.karna@gmail.com>
2020-01-29 08:38:09 +0200

    Compiled gRPC clients for rest-api (images & auth)

--- rest-api/src/client/generated/auth_service_grpc_pb.js
+++ rest-api/src/client/generated/auth_service_grpc_pb.js
@@ -4,91 +4,91 @@
 var grpc = require('grpc');
 var auth_service_pb = require('./auth_service_pb.js');
 
-function serialize_AccountInfo(arg) {
+function serialize_Auth_AccountInfo(arg) {
   if (!(arg instanceof auth_service_pb.AccountInfo)) {
-    throw new Error('Expected argument of type AccountInfo');
+    throw new Error('Expected argument of type Auth.AccountInfo');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_AccountInfo(buffer_arg) {
+function deserialize_Auth_AccountInfo(buffer_arg) {
   return auth_service_pb.AccountInfo.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_AccountRequest(arg) {
+function serialize_Auth_AccountRequest(arg) {
   if (!(arg instanceof auth_service_pb.AccountRequest)) {
-    throw new Error('Expected argument of type AccountRequest');
+    throw new Error('Expected argument of type Auth.AccountRequest');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_AccountRequest(buffer_arg) {
+function deserialize_Auth_AccountRequest(buffer_arg) {
   return auth_service_pb.AccountRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_JWTTokens(arg) {
-  if (!(arg instanceof auth_service_pb.JWTTokens)) {
-    throw new Error('Expected argument of type JWTTokens');
-  }
-  return Buffer.from(arg.serializeBinary());
-}
-
-function deserialize_JWTTokens(buffer_arg) {
-  return auth_service_pb.JWTTokens.deserializeBinary(new Uint8Array(buffer_arg));
-}
-
-function serialize_NewAccount(arg) {
+function serialize_Auth_NewAccount(arg) {
   if (!(arg instanceof auth_service_pb.NewAccount)) {
-    throw new Error('Expected argument of type NewAccount');
+    throw new Error('Expected argument of type Auth.NewAccount');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_NewAccount(buffer_arg) {
+function deserialize_Auth_NewAccount(buffer_arg) {
   return auth_service_pb.NewAccount.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_RenewRequest(arg) {
+function serialize_Auth_RenewRequest(arg) {
   if (!(arg instanceof auth_service_pb.RenewRequest)) {
-    throw new Error('Expected argument of type RenewRequest');
+    throw new Error('Expected argument of type Auth.RenewRequest');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_RenewRequest(buffer_arg) {
+function deserialize_Auth_RenewRequest(buffer_arg) {
   return auth_service_pb.RenewRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_SignInResponse(arg) {
-  if (!(arg instanceof auth_service_pb.SignInResponse)) {
-    throw new Error('Expected argument of type SignInResponse');
+function serialize_Auth_RenewResponse(arg) {
+  if (!(arg instanceof auth_service_pb.RenewResponse)) {
+    throw new Error('Expected argument of type Auth.RenewResponse');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_SignInResponse(buffer_arg) {
+function deserialize_Auth_RenewResponse(buffer_arg) {
+  return auth_service_pb.RenewResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_Auth_SignInResponse(arg) {
+  if (!(arg instanceof auth_service_pb.SignInResponse)) {
+    throw new Error('Expected argument of type Auth.SignInResponse');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_Auth_SignInResponse(buffer_arg) {
   return auth_service_pb.SignInResponse.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_SignUpResponse(arg) {
+function serialize_Auth_SignUpResponse(arg) {
   if (!(arg instanceof auth_service_pb.SignUpResponse)) {
-    throw new Error('Expected argument of type SignUpResponse');
+    throw new Error('Expected argument of type Auth.SignUpResponse');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_SignUpResponse(buffer_arg) {
+function deserialize_Auth_SignUpResponse(buffer_arg) {
   return auth_service_pb.SignUpResponse.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_UserCredentials(arg) {
+function serialize_Auth_UserCredentials(arg) {
   if (!(arg instanceof auth_service_pb.UserCredentials)) {
-    throw new Error('Expected argument of type UserCredentials');
+    throw new Error('Expected argument of type Auth.UserCredentials');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_UserCredentials(buffer_arg) {
+function deserialize_Auth_UserCredentials(buffer_arg) {
   return auth_service_pb.UserCredentials.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
@@ -97,51 +97,51 @@ function deserialize_UserCredentials(buffer_arg) {
 var AuthService = exports.AuthService = {
   // Sign up a new user account
 signUp: {
-    path: '/Auth/SignUp',
+    path: '/Auth.Auth/SignUp',
     requestStream: false,
     responseStream: false,
     requestType: auth_service_pb.NewAccount,
     responseType: auth_service_pb.SignUpResponse,
-    requestSerialize: serialize_NewAccount,
-    requestDeserialize: deserialize_NewAccount,
-    responseSerialize: serialize_SignUpResponse,
-    responseDeserialize: deserialize_SignUpResponse,
+    requestSerialize: serialize_Auth_NewAccount,
+    requestDeserialize: deserialize_Auth_NewAccount,
+    responseSerialize: serialize_Auth_SignUpResponse,
+    responseDeserialize: deserialize_Auth_SignUpResponse,
   },
   // Sign the user in, get proper tokens as response
 signIn: {
-    path: '/Auth/SignIn',
+    path: '/Auth.Auth/SignIn',
     requestStream: false,
     responseStream: false,
     requestType: auth_service_pb.UserCredentials,
     responseType: auth_service_pb.SignInResponse,
-    requestSerialize: serialize_UserCredentials,
-    requestDeserialize: deserialize_UserCredentials,
-    responseSerialize: serialize_SignInResponse,
-    responseDeserialize: deserialize_SignInResponse,
+    requestSerialize: serialize_Auth_UserCredentials,
+    requestDeserialize: deserialize_Auth_UserCredentials,
+    responseSerialize: serialize_Auth_SignInResponse,
+    responseDeserialize: deserialize_Auth_SignInResponse,
   },
   // Get a new pair of access and refresh tokens
 renewToken: {
-    path: '/Auth/RenewToken',
+    path: '/Auth.Auth/RenewToken',
     requestStream: false,
     responseStream: false,
     requestType: auth_service_pb.RenewRequest,
-    responseType: auth_service_pb.JWTTokens,
-    requestSerialize: serialize_RenewRequest,
-    requestDeserialize: deserialize_RenewRequest,
-    responseSerialize: serialize_JWTTokens,
-    responseDeserialize: deserialize_JWTTokens,
+    responseType: auth_service_pb.RenewResponse,
+    requestSerialize: serialize_Auth_RenewRequest,
+    requestDeserialize: deserialize_Auth_RenewRequest,
+    responseSerialize: serialize_Auth_RenewResponse,
+    responseDeserialize: deserialize_Auth_RenewResponse,
   },
   // Get account details based on access token
 getAccount: {
-    path: '/Auth/GetAccount',
+    path: '/Auth.Auth/GetAccount',
     requestStream: false,
     responseStream: false,
     requestType: auth_service_pb.AccountRequest,
     responseType: auth_service_pb.AccountInfo,
-    requestSerialize: serialize_AccountRequest,
-    requestDeserialize: deserialize_AccountRequest,
-    responseSerialize: serialize_AccountInfo,
-    responseDeserialize: deserialize_AccountInfo,
+    requestSerialize: serialize_Auth_AccountRequest,
+    requestDeserialize: deserialize_Auth_AccountRequest,
+    responseSerialize: serialize_Auth_AccountInfo,
+    responseDeserialize: deserialize_Auth_AccountInfo,
   },
 };
 
---
+++ rest-api/src/client/generated/image_service_pb.d.ts
@@ -0,0 +1,577 @@
+// package: Image
+// file: image_service.proto
+
+/* tslint:disable */
+/* eslint-disable */
+
+import * as jspb from "google-protobuf";
+
+export class CreateImageRequest extends jspb.Message { 
+    getCaption(): string;
+    setCaption(value: string): void;
+
+    getCreatorId(): string;
+    setCreatorId(value: string): void;
+
+    getData(): Uint8Array | string;
+    getData_asU8(): Uint8Array;
+    getData_asB64(): string;
+    setData(value: Uint8Array | string): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): CreateImageRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: CreateImageRequest): CreateImageRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: CreateImageRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): CreateImageRequest;
+    static deserializeBinaryFromReader(message: CreateImageRequest, reader: jspb.BinaryReader): CreateImageRequest;
+}
+
+export namespace CreateImageRequest {
+    export type AsObject = {
+        caption: string,
+        creatorId: string,
+        data: Uint8Array | string,
+    }
+}
+
+export class CreateImageResponse extends jspb.Message { 
+
+    hasImage(): boolean;
+    clearImage(): void;
+    getImage(): Image | undefined;
+    setImage(value?: Image): void;
+
+
+    hasError(): boolean;
+    clearError(): void;
+    getError(): CreateImageErrorStatus;
+    setError(value: CreateImageErrorStatus): void;
+
+
+    getStatusCase(): CreateImageResponse.StatusCase;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): CreateImageResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: CreateImageResponse): CreateImageResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: CreateImageResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): CreateImageResponse;
+    static deserializeBinaryFromReader(message: CreateImageResponse, reader: jspb.BinaryReader): CreateImageResponse;
+}
+
+export namespace CreateImageResponse {
+    export type AsObject = {
+        image?: Image.AsObject,
+        error: CreateImageErrorStatus,
+    }
+
+    export enum StatusCase {
+        STATUS_NOT_SET = 0,
+    
+    IMAGE = 1,
+
+    ERROR = 2,
+
+    }
+
+}
+
+export class DeleteImageRequest extends jspb.Message { 
+    getId(): string;
+    setId(value: string): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): DeleteImageRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: DeleteImageRequest): DeleteImageRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: DeleteImageRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): DeleteImageRequest;
+    static deserializeBinaryFromReader(message: DeleteImageRequest, reader: jspb.BinaryReader): DeleteImageRequest;
+}
+
+export namespace DeleteImageRequest {
+    export type AsObject = {
+        id: string,
+    }
+}
+
+export class DeleteImageResponse extends jspb.Message { 
+    getStatus(): DeleteImageStatus;
+    setStatus(value: DeleteImageStatus): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): DeleteImageResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: DeleteImageResponse): DeleteImageResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: DeleteImageResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): DeleteImageResponse;
+    static deserializeBinaryFromReader(message: DeleteImageResponse, reader: jspb.BinaryReader): DeleteImageResponse;
+}
+
+export namespace DeleteImageResponse {
+    export type AsObject = {
+        status: DeleteImageStatus,
+    }
+}
+
+export class GetImageRequest extends jspb.Message { 
+    getImageId(): string;
+    setImageId(value: string): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): GetImageRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: GetImageRequest): GetImageRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: GetImageRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): GetImageRequest;
+    static deserializeBinaryFromReader(message: GetImageRequest, reader: jspb.BinaryReader): GetImageRequest;
+}
+
+export namespace GetImageRequest {
+    export type AsObject = {
+        imageId: string,
+    }
+}
+
+export class GetImageResponse extends jspb.Message { 
+
+    hasImage(): boolean;
+    clearImage(): void;
+    getImage(): Image | undefined;
+    setImage(value?: Image): void;
+
+
+    hasError(): boolean;
+    clearError(): void;
+    getError(): GetImageErrorStatus;
+    setError(value: GetImageErrorStatus): void;
+
+
+    getStatusCase(): GetImageResponse.StatusCase;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): GetImageResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: GetImageResponse): GetImageResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: GetImageResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): GetImageResponse;
+    static deserializeBinaryFromReader(message: GetImageResponse, reader: jspb.BinaryReader): GetImageResponse;
+}
+
+export namespace GetImageResponse {
+    export type AsObject = {
+        image?: Image.AsObject,
+        error: GetImageErrorStatus,
+    }
+
+    export enum StatusCase {
+        STATUS_NOT_SET = 0,
+    
+    IMAGE = 1,
+
+    ERROR = 2,
+
+    }
+
+}
+
+export class GetImageDataRequest extends jspb.Message { 
+    getImageId(): string;
+    setImageId(value: string): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): GetImageDataRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: GetImageDataRequest): GetImageDataRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: GetImageDataRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): GetImageDataRequest;
+    static deserializeBinaryFromReader(message: GetImageDataRequest, reader: jspb.BinaryReader): GetImageDataRequest;
+}
+
+export namespace GetImageDataRequest {
+    export type AsObject = {
+        imageId: string,
+    }
+}
+
+export class GetImageDataResponse extends jspb.Message { 
+
+    hasData(): boolean;
+    clearData(): void;
+    getData(): Uint8Array | string;
+    getData_asU8(): Uint8Array;
+    getData_asB64(): string;
+    setData(value: Uint8Array | string): void;
+
+
+    hasError(): boolean;
+    clearError(): void;
+    getError(): GetImageErrorStatus;
+    setError(value: GetImageErrorStatus): void;
+
+
+    getStatusCase(): GetImageDataResponse.StatusCase;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): GetImageDataResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: GetImageDataResponse): GetImageDataResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: GetImageDataResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): GetImageDataResponse;
+    static deserializeBinaryFromReader(message: GetImageDataResponse, reader: jspb.BinaryReader): GetImageDataResponse;
+}
+
+export namespace GetImageDataResponse {
+    export type AsObject = {
+        data: Uint8Array | string,
+        error: GetImageErrorStatus,
+    }
+
+    export enum StatusCase {
+        STATUS_NOT_SET = 0,
+    
+    DATA = 1,
+
+    ERROR = 2,
+
+    }
+
+}
+
+export class GetUserImagesRequest extends jspb.Message { 
+    getUserId(): string;
+    setUserId(value: string): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): GetUserImagesRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: GetUserImagesRequest): GetUserImagesRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: GetUserImagesRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): GetUserImagesRequest;
+    static deserializeBinaryFromReader(message: GetUserImagesRequest, reader: jspb.BinaryReader): GetUserImagesRequest;
+}
+
+export namespace GetUserImagesRequest {
+    export type AsObject = {
+        userId: string,
+    }
+}
+
+export class GetUserImagesResponse extends jspb.Message { 
+
+    hasPage(): boolean;
+    clearPage(): void;
+    getPage(): UserImagePage | undefined;
+    setPage(value?: UserImagePage): void;
+
+
+    hasError(): boolean;
+    clearError(): void;
+    getError(): GetUserImagesErrorStatus;
+    setError(value: GetUserImagesErrorStatus): void;
+
+
+    getStatusCase(): GetUserImagesResponse.StatusCase;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): GetUserImagesResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: GetUserImagesResponse): GetUserImagesResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: GetUserImagesResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): GetUserImagesResponse;
+    static deserializeBinaryFromReader(message: GetUserImagesResponse, reader: jspb.BinaryReader): GetUserImagesResponse;
+}
+
+export namespace GetUserImagesResponse {
+    export type AsObject = {
+        page?: UserImagePage.AsObject,
+        error: GetUserImagesErrorStatus,
+    }
+
+    export enum StatusCase {
+        STATUS_NOT_SET = 0,
+    
+    PAGE = 1,
+
+    ERROR = 2,
+
+    }
+
+}
+
+export class SearchImagesRequest extends jspb.Message { 
+
+    hasHashTag(): boolean;
+    clearHashTag(): void;
+    getHashTag(): string;
+    setHashTag(value: string): void;
+
+
+    hasUserTag(): boolean;
+    clearUserTag(): void;
+    getUserTag(): string;
+    setUserTag(value: string): void;
+
+
+    getSearchCase(): SearchImagesRequest.SearchCase;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): SearchImagesRequest.AsObject;
+    static toObject(includeInstance: boolean, msg: SearchImagesRequest): SearchImagesRequest.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: SearchImagesRequest, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): SearchImagesRequest;
+    static deserializeBinaryFromReader(message: SearchImagesRequest, reader: jspb.BinaryReader): SearchImagesRequest;
+}
+
+export namespace SearchImagesRequest {
+    export type AsObject = {
+        hashTag: string,
+        userTag: string,
+    }
+
+    export enum SearchCase {
+        SEARCH_NOT_SET = 0,
+    
+    HASH_TAG = 1,
+
+    USER_TAG = 2,
+
+    }
+
+}
+
+export class SearchImagesResponse extends jspb.Message { 
+
+    hasPage(): boolean;
+    clearPage(): void;
+    getPage(): ImageSearchPage | undefined;
+    setPage(value?: ImageSearchPage): void;
+
+
+    hasError(): boolean;
+    clearError(): void;
+    getError(): SearchImagesErrorStatus;
+    setError(value: SearchImagesErrorStatus): void;
+
+
+    getStatusCase(): SearchImagesResponse.StatusCase;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): SearchImagesResponse.AsObject;
+    static toObject(includeInstance: boolean, msg: SearchImagesResponse): SearchImagesResponse.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: SearchImagesResponse, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): SearchImagesResponse;
+    static deserializeBinaryFromReader(message: SearchImagesResponse, reader: jspb.BinaryReader): SearchImagesResponse;
+}
+
+export namespace SearchImagesResponse {
+    export type AsObject = {
+        page?: ImageSearchPage.AsObject,
+        error: SearchImagesErrorStatus,
+    }
+
+    export enum StatusCase {
+        STATUS_NOT_SET = 0,
+    
+    PAGE = 1,
+
+    ERROR = 2,
+
+    }
+
+}
+
+export class ImageSearchPage extends jspb.Message { 
+
+    hasHashTag(): boolean;
+    clearHashTag(): void;
+    getHashTag(): string;
+    setHashTag(value: string): void;
+
+
+    hasUserTag(): boolean;
+    clearUserTag(): void;
+    getUserTag(): string;
+    setUserTag(value: string): void;
+
+    getSize(): number;
+    setSize(value: number): void;
+
+    getPage(): number;
+    setPage(value: number): void;
+
+    getTotalCount(): number;
+    setTotalCount(value: number): void;
+
+    clearImagesList(): void;
+    getImagesList(): Array<Image>;
+    setImagesList(value: Array<Image>): void;
+    addImages(value?: Image, index?: number): Image;
+
+
+    getSearchCase(): ImageSearchPage.SearchCase;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): ImageSearchPage.AsObject;
+    static toObject(includeInstance: boolean, msg: ImageSearchPage): ImageSearchPage.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: ImageSearchPage, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): ImageSearchPage;
+    static deserializeBinaryFromReader(message: ImageSearchPage, reader: jspb.BinaryReader): ImageSearchPage;
+}
+
+export namespace ImageSearchPage {
+    export type AsObject = {
+        hashTag: string,
+        userTag: string,
+        size: number,
+        page: number,
+        totalCount: number,
+        imagesList: Array<Image.AsObject>,
+    }
+
+    export enum SearchCase {
+        SEARCH_NOT_SET = 0,
+    
+    HASH_TAG = 1,
+
+    USER_TAG = 2,
+
+    }
+
+}
+
+export class UserImagePage extends jspb.Message { 
+    getUserId(): string;
+    setUserId(value: string): void;
+
+    getSize(): number;
+    setSize(value: number): void;
+
+    getPage(): number;
+    setPage(value: number): void;
+
+    getTotalCount(): number;
+    setTotalCount(value: number): void;
+
+    clearImagesList(): void;
+    getImagesList(): Array<Image>;
+    setImagesList(value: Array<Image>): void;
+    addImages(value?: Image, index?: number): Image;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): UserImagePage.AsObject;
+    static toObject(includeInstance: boolean, msg: UserImagePage): UserImagePage.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: UserImagePage, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): UserImagePage;
+    static deserializeBinaryFromReader(message: UserImagePage, reader: jspb.BinaryReader): UserImagePage;
+}
+
+export namespace UserImagePage {
+    export type AsObject = {
+        userId: string,
+        size: number,
+        page: number,
+        totalCount: number,
+        imagesList: Array<Image.AsObject>,
+    }
+}
+
+export class Image extends jspb.Message { 
+    getId(): string;
+    setId(value: string): void;
+
+    getType(): string;
+    setType(value: string): void;
+
+    getWidth(): number;
+    setWidth(value: number): void;
+
+    getHeight(): number;
+    setHeight(value: number): void;
+
+    getUserId(): string;
+    setUserId(value: string): void;
+
+    getCaption(): string;
+    setCaption(value: string): void;
+
+    getCreatedAt(): string;
+    setCreatedAt(value: string): void;
+
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): Image.AsObject;
+    static toObject(includeInstance: boolean, msg: Image): Image.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: Image, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): Image;
+    static deserializeBinaryFromReader(message: Image, reader: jspb.BinaryReader): Image;
+}
+
+export namespace Image {
+    export type AsObject = {
+        id: string,
+        type: string,
+        width: number,
+        height: number,
+        userId: string,
+        caption: string,
+        createdAt: string,
+    }
+}
+
+export enum CreateImageErrorStatus {
+    CREATE_IMAGE_SERVER_ERROR = 0,
+    CONTENT_TOO_LONG = 1,
+    CREATOR_NOT_FOUND = 2,
+    INVALID_DATA = 3,
+    CAPTION_TOO_LONG = 4,
+}
+
+export enum DeleteImageStatus {
+    DELETE_IMAGE_SERVER_ERROR = 0,
+    DELETABLE_NOT_FOUND = 1,
+    OK = 2,
+}
+
+export enum GetImageErrorStatus {
+    GET_IMAGE_SERVER_ERROR = 0,
+    IMAGE_NOT_FOUND = 1,
+}
+
+export enum GetUserImagesErrorStatus {
+    GET_USER_IMAGES_SERVER_ERROR = 0,
+    USER_NOT_FOUND = 1,
+}
+
+export enum SearchImagesErrorStatus {
+    SEARCH_IMAGES_SERVER_ERROR = 0,
+    EMPTY_SEARCH = 1,
+}
