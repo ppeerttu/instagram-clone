afee5ea363a1ae805c93f7efa579c010e74ce4db
Juho Kantola <kirvesmies96@gmail.com>
2020-01-29 10:15:52 +0200

    - add mongo configuration

---
+++ comment-service/src/main/kotlin/com/instagram_clone/comment_service/Constants.java
@@ -0,0 +1,13 @@
+package com.instagram_clone.comment_service;
+
+public class Constants {
+
+  public static String MONGO_KEY_USER = "MONGO_USER";
+  public static String MONGO_KEY_PASSWORD = "MONGO_PASSWORD";
+  public static String MONGO_KEY_DB_NAME = "MONGO_DB_NAME";
+  public static String MONGO_KEY_PORT = "MONGO_PORT";
+  public static String MONGO_KEY_HOST = "MONGO_HOST";
+
+  public static String GRPC_KEY_HOST = "GRPC_HOST";
+  public static String GRPC_KEY_PORT = "GRPC_PORT";
+}
---
+++ comment-service/src/main/kotlin/com/instagram_clone/comment_service/service/CommentServiceMongoImpl.kt
@@ -0,0 +1,5 @@
+package com.instagram_clone.comment_service.service
+
+//class CommentServiceMongoImpl() : CommentService {
+//
+//}
