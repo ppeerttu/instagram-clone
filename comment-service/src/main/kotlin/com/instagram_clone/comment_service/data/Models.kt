3011657a872469412482ba7887fa2d59aba14857
Juho Kantola <kirvesmies96@gmail.com>
2020-02-01 09:44:46 +0200

    minor code cleanup

--- comment-service/src/main/kotlin/com/instagram_clone/comment_service/data/Models.kt
+++ comment-service/src/main/kotlin/com/instagram_clone/comment_service/data/Models.kt
@@ -1,10 +1,7 @@
 package com.instagram_clone.comment_service.data
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.JsonProperty
 
-// this annotation is needed to make jacksonxml json serialization work with this kind of kotlin data class
-//@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 data class CommentWrapper(
   // Mark id as _id, because mongodb needs it and wont create new field this way
   @get:JsonProperty("_id") var id: String = "",
